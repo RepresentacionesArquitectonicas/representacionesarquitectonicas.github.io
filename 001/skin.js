4e24b480438a1bdda30fd8c74306943cca485be1
RepresentacionesArquitectonicas <94709586+RepresentacionesArquitectonicas@users.noreply.github.com>
2021-11-19 16:55:03 -0600

    Update skin.js

--- 001/skin.js
+++ 001/skin.js
@@ -388,7 +388,7 @@ function pano2vrSkin(player,base) {
 					me._map_1.ggHMarkerAnchorOffset = 20;
 					me._map_1.ggVMarkerAnchorOffset = 40;
 					var marker = document.createElement('img');
-					marker.setAttribute('src', basePath + 'images/_ggMapPin.png');
+					marker.setAttribute('src', basePath + 'images/ggMapPin.png');
 					marker.setAttribute('title', player.getNodeTitle(id));
 					marker.style['width'] = '40px';
 					marker.style['width'] = '40px';
